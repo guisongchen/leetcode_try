d26d3efde3301e6b3da0a3d996a45f48649ee613
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-02-28 20:06:00 +0800

    Create 15_3Sum.cpp

---
+++ 1-50/15_3Sum.cpp
@@ -0,0 +1,73 @@
+/*
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // define return
+        vector<vector<int>> ret;
+        // null case
+        if (nums.size() == 0)
+            return ret;
+        const int n = nums.size();
+        
+        // sort to fix one of three elements
+        sort(nums.begin(), nums.end());
+        
+        // do loop
+        for (int i = 0; i < n; i++) {
+            int& fix = nums[i];
+            if (fix > 0)
+                break;
+            // handle redundant for fix element
+            if (i > 0 && nums[i] == nums[i-1])
+                continue;
+            
+            int lt = i+1;
+            int rt = n-1;
+            
+            while (lt < rt) {
+                int& lv = nums[lt];
+                int& rv = nums[rt];
+                
+                if (lv+rv == -fix) {
+                    vector cur{fix, lv, rv};
+                    ret.push_back(cur);
+                    lt++;
+                    rt--;
+                    
+                    // handle redundant for left and right elements
+                    while (lt < rt && nums[lt] == nums[lt-1])
+                        lt++;
+                    while (lt < rt && nums[rt] == nums[rt+1])
+                        rt--;
+                } else if (lv+rv < -fix) {
+                    lt++;
+                    while (lt < rt && nums[lt] == nums[lt-1])
+                        lt++;
+                } else { 
+                    rt--;
+                    while (lt < rt && nums[rt] == nums[rt+1])
+                        rt--;
+                }
+            }
+        }
+        return ret;
+    }
+};
