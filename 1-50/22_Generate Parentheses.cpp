ab62bbe1c2f7a76cdf78fb8a12237e2ac08b4283
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-02-29 13:34:58 +0800

    Create 22_Generate Parentheses.cpp

---
+++ 1-50/22_Generate Parentheses.cpp
@@ -0,0 +1,50 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+*/
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        // define return value
+        vector<string> ret;
+        // null case
+        if (n < 1)
+            return ret;
+        
+        // string s = {"("}; 
+        // int left = 1, right = 0;
+        // it must be '(', left = 1
+        dfs(ret, n, "(", 1, 0);
+        return ret;
+    }
+    
+private:
+    // NOT use reference to avoid backtrace
+    void dfs(vector<string>& ret, const int& n, 
+             string s, int left, int right) {
+        if (left == n && right == n) {
+            ret.push_back(s);
+            return;
+        }
+        
+        // if left < n, free to use left
+        // since pass by value, avoid backtrace(s.pop_back(), left--)
+        if (left < n) 
+            dfs(ret, n, s+'(', left+1, right);
+        
+        // if right < left, free to use right
+        if (right < left) 
+            dfs(ret, n, s+')', left, right+1);
+    }
+    
+};
