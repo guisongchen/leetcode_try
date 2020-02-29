c1fa99cc57f3ab2d5e36d0cd77e9a0130045ccd7
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-02-29 20:15:48 +0800

    Create 49_Group Anagrams.cpp

---
+++ 1-50/49_Group Anagrams.cpp
@@ -0,0 +1,58 @@
+/*
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+*/
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // use bit operater is not reasonable
+        // "ab" "aab" are different
+        // but bit |= each_of_"ab" and bit |= each_of_"aab" are the same
+        
+        // null case
+        if (strs.size() < 2)
+            return vector<vector<string>> {strs};
+        
+        // define return
+        vector<vector<string>> ret;
+        // use second "int" to trace ret size 
+        // each key_string has a unique int(since int change with ret size)
+        unordered_map<string, int> hashStr;
+        
+        for (int i = 0, n= strs.size(); i < n; i++) {
+            
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            
+            // if not exist, need to add one null space for ret
+            // e.g. first string with int = 0, which also the first index of ret
+            if (!hashStr.count(s)) {
+                // bond key string to ret size
+                hashStr[s] = ret.size();
+                
+                // create new space
+                ret.push_back({});
+            }
+            
+            // now ret[hashStr[s]] should exist OR have been created before
+            ret[hashStr[s]].push_back(strs[i]);
+        }
+        
+        return ret;
+        
+    }
+};
