8ba9293f117f6c9c89ab3d4fcad9d731d4b6c5cd
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-01 14:10:08 +0800

    Update 55_Jump Game.cpp

--- 51-100/55_Jump Game.cpp
+++ 51-100/55_Jump Game.cpp
@@ -18,6 +18,37 @@ Explanation: You will always arrive at index 3 no matter what. Its maximum
              jump length is 0, which makes it impossible to reach the last index.
 */
 
+
+
+// keypoints:
+// 1. use greedy algorithm
+// 2. transform formular:  reach = max(reach, i+nums[i])
+//    reach: max reachable index(aka skip index i)
+//    i+nums[i] : start from index i
+
+// corner case:
+// 1. i > reach, means can't reach index i, which means false
+// 2. reach >= n-1, means we can reach ending, should be true
+//    using ">=" means equal or beyond ending both are true
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        // Greedy
+        int n = nums.size();
+        int reach = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            if (i > reach || reach >= n-1)
+                return reach >= n-1;
+            reach = max(i+nums[i], reach);
+        }
+        
+        return reach >= n-1;
+    }
+};
+
+
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
