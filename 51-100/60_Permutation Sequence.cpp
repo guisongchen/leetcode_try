93a4287737d3c303a8cb143c05147fdd78d30e75
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-01 15:54:41 +0800

    Update 60_Permutation Sequence.cpp

--- 51-100/60_Permutation Sequence.cpp
+++ 51-100/60_Permutation Sequence.cpp
@@ -25,43 +25,60 @@ Input: n = 4, k = 9
 Output: "2314"
 */
 
+// keypoints:
+// 1. find the rule:
+
+/*
+a1 = k / (n - 1)!
+k1 = k
+
+a2 = k1 / (n - 2)!
+k2 = k1 % (n - 2)!
+...
+
+an-1 = kn-2 / 1!
+kn-1 = kn-2 % 1!
+
+an = kn-1 / 0!
+kn = kn-1 % 0!
+*/
+
+// 2. translate index to array index(which start from 0)
 
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        string ret(n, '1');
-        vector<int> nums(n, 1);
+        string ret;
+        string nums = "123456789";
         vector<int> factor(n, 1);
         
         // get directory of n!
         // get nums from 1 to n
         for (int i = 1; i < n; i++) {
             factor[i] = factor[i-1] * i;
-            // i from 0 to n-1, nums[i] form 1 to n
-            nums[i] = i + 1;
         }
         
-        // factor start form 0, while k>=1, aligned by k--
+        // factor start from 0, while k>=1, turn k into array index by k-- 
         // also because we use '/' and '%' later, which all start from 0
-        // so k MUST start form 0
+        // so k MUST start from 0
         k--;
         
-        auto it_begin = nums.begin();
         for (int i = n; i >= 1; i--) {
+            
             // for the i-th digit, it has (i-1)! permutations
             // which forms: i * (i-1)! permutation
             // locate i-th idx by divide (i-1)!
             int idx = k / (factor[i-1]);
             
-            // push into result(update init '1' with num)
-            // since k from 0 to n-1; n from 1 to n; k+'1' map to nums{1,2,3...}
-            ret[n-i] = nums[idx] + '0';
-            // erase chosen digit from nums
-            nums.erase(it_begin+idx);
-            
             // after (n-1)! permutations, remian: k' = k % (factor[i-1])
             // update for next loop
             k =  k % (factor[i-1]);
+            
+            // push into result
+            ret.push_back(nums[idx]);
+            // erase chosen digit from nums
+            nums.erase(idx, 1);
+            
         }
         
         return ret;
