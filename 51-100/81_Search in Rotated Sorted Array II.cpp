7d63d99f9a2e1c0f78036bdab7e85f91e850471f
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-02 14:23:23 +0800

    Update 81_Search in Rotated Sorted Array II.cpp

--- 51-100/81_Search in Rotated Sorted Array II.cpp
+++ 51-100/81_Search in Rotated Sorted Array II.cpp
@@ -19,6 +19,10 @@ This is a follow up problem to Search in Rotated Sorted Array, where nums may co
 Would this affect the run-time complexity? How and why?
 */
 
+// keypoints:
+// 1. difference with Search in Rotated Sorted Array: may contain duplicates
+//    ===>   if (nums[mid] == nums[right] --right;
+
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
