fddee34e5a3943e103c22ea65a9a658264c0a72b
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-02 19:51:50 +0800

    Create 102_Binary Tree Level Order Traversal.cpp

---
+++ 101-150/102_Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,65 @@
+/*
+
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        // keypoint is how to decide a layer is finished
+        if (!root)
+            return vector<vector<int>>{};
+        
+        vector<vector<int>> ret;
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()) {
+        
+            // now all elements in que is on same layer
+            int n = que.size();
+            vector<int> cur;
+            
+            // use n to control pop times, make sure only pop current nodes
+            // use queue to make sure next layer nodes are added at the end
+            while (n--) {
+                TreeNode* t = que.front();
+                cur.push_back(t->val);
+                que.pop();
+                
+                if (t->left)
+                    que.push(t->left);
+                if (t->right)
+                    que.push(t->right);
+            }
+            
+            // save current layer in result
+            ret.push_back(cur);
+        }
+        
+        return ret;
+    }
+};
