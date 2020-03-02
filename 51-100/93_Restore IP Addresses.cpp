b9c6aa3604097d53a7701219fcb67bff3898eb01
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-02 17:09:16 +0800

    Create 93_Restore IP Addresses.cpp

---
+++ 51-100/93_Restore IP Addresses.cpp
@@ -0,0 +1,62 @@
+/*
+
+Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+Example:
+
+Input: "25525511135"
+Output: ["255.255.11.135", "255.255.111.35"]
+
+*/
+
+
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        // dfs
+        vector<string> ret;
+        string cur;
+        dfs(s, ret, cur, s.size(), 0);
+        return ret;
+    }
+    
+    void dfs(string& s, vector<string>& ret, string& cur, int remain, int cnt) {
+        
+        // all digit from s are used
+        if (cnt >= 4) {
+            if(cnt == 4 && remain == 0){
+                ret.push_back(cur);
+            }
+            
+            return;
+        }
+            
+        string backup = cur;
+        
+        for (int i = 1; i < 4; i++) {
+
+            // substr needs to be checked
+            string tmp = s.substr(cur.size()-cnt, i);
+
+            if (checkRange(tmp)) {
+                
+                if (cnt != 3)
+                    cur += tmp + ".";
+                else  // the last part NOT need '.'
+                    cur += tmp;
+                
+                dfs(s, ret, cur, remain-i, cnt+1);
+                cur = backup;
+            }
+        }
+    }
+    
+    bool checkRange(string& s) { 
+        // corner case: 010
+        if (s.size() > 1 && s[0] == '0')
+            return false;
+        
+        int res = atoi(s.c_str());
+        return res >= 0 && res <= 255;
+    }
+};
