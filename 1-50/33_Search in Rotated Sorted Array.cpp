7ae7eabfa204b6013c2af301d8851a399b7eebe0
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-02 14:11:04 +0800

    Update 33_Search in Rotated Sorted Array.cpp

--- 1-50/33_Search in Rotated Sorted Array.cpp
+++ 1-50/33_Search in Rotated Sorted Array.cpp
@@ -21,6 +21,48 @@ Output: -1
 
 */
 
+// keypoints:
+// 1. using binary search
+// 2. since array is rotated, needs to find which part is in sequence
+// 3. use three pointers: left, mid, right
+//
+// use mid and right pointers to find which part is sorted
+// mid < right:   right part is in sequence
+// mid > right:   left part is in sequence
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        
+        while (left <= right) {
+            
+            int mid = left + (right-left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < nums[right]) { // right in sequence
+                if (nums[mid] < target && nums[right] >= target) { // compare right head and tail
+                    left = mid + 1; // update left, so "=" should at right side
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] > nums[right]) { // left in sequence
+                if (nums[mid] > target && nums[left] <= target) { // compare left head and tail
+                    right = mid -1 ; // update right, so "=" should at left side
+                } else {
+                    left = mid + 1;
+                }
+            } else { // nums[mid] == nums[right]
+                return -1;
+            }
+        }
+        
+        return -1;
+        
+    }
+};
+
 
 class Solution {
 public:
