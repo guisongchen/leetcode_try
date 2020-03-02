46a9581b8e3a9f7cb59ff0ef774e7338f8a56c86
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-02 10:21:40 +0800

    Create 77_Combinations.cpp

---
+++ 51-100/77_Combinations.cpp
@@ -0,0 +1,51 @@
+/*
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+Example:
+
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+*/
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        // corner case
+        if (n < 1 || k < 1)
+            return vector<vector<int>>{};
+        
+        vector<vector<int>> ret;
+        vector<int> cur(k, 0);
+        
+        // notice the fact:
+        // 1. the first elements add 1 after the last elements over k
+        // 2. the first elements should not over n-k+1
+        
+        int i = 0;
+        while (i >= 0) {
+            // update first element
+            cur[i]++;
+            
+            if (cur[i] > n) { // over n, retrive back to last position
+                i--;
+            } else if (i == k-1) {
+                ret.emplace_back(cur);
+            } else {
+                // move i to i+1, then update i(plus one)
+                cur[i+1] = cur[i];
+                i++;
+            }
+        }
+        
+        return ret;
+    }
+    
+};
