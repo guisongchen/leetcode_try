d0076fe36665770eb430ccd2d078a75bb881bdf9
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-02 09:26:40 +0800

    Create 74_Search a 2D Matrix.cpp

---
+++ 51-100/74_Search a 2D Matrix.cpp
@@ -0,0 +1,58 @@
+/*
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // NULL case
+        if (matrix.size() == 0 || matrix[0].size() == 0)
+            return false;
+        
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        // binary search, first locate row, then search in row
+        // OR we can just consider matrix as one-dim array, which needs to handle index
+        // 
+        // OR we can use double pointer
+        
+        int i = 0;
+        int j = col-1;
+        
+        while (i < row && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) { // move to next row
+                i++;
+            } else { // move to left
+                j--;
+            }
+        }
+        
+        return false;
+    }
+};
