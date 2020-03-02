e3c7393524da541a1918951fb542f97a5882c86b
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-02 14:39:51 +0800

    Create 86_Partition List.cpp

---
+++ 51-100/86_Partition List.cpp
@@ -0,0 +1,74 @@
+/*
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Example:
+
+Input: head = 1->4->3->2->5->2, x = 3
+Output: 1->2->2->4->3->5
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        // basic idea:
+        // if value less than x, hold steady, 
+        // else check next until find node less than x
+        // link that node between cur and original next
+        
+        // corner case
+        if (!head)
+            return head;
+        
+        // in case the first node is greater than x, use deamon node
+        ListNode* deamon = new ListNode(0);
+        deamon->next = head;
+        
+        ListNode* last = deamon;
+        
+        // big picture:
+        //  last  cur    prev target   
+        //   |    |       |   |
+        //   1    4   3   5   2   2
+        //      ^             |
+        //      |_____________|
+        //
+        
+        while (last->next) {
+            ListNode* cur = last->next;
+            
+            // find next one which less than x
+            ListNode* prev = last;  // prev node of target node
+            while (cur && cur->val >= x) {
+                prev = cur;
+                cur = cur->next;
+            }
+            
+            // if cur was moved
+            // NOTE: cur may be null
+            if (cur != last->next) {
+                if (cur) {
+                    prev->next = cur->next;
+                    cur->next = last->next;
+                    last->next = cur;
+                } else {
+                    break;
+                }
+            } else {
+                last = cur;
+            }
+        }
+        
+        return deamon->next;
+    }
+};
