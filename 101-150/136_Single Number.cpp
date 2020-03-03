dc83332adef70e642f2db9d9b44a34e4fa8e6a18
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-03 15:44:44 +0800

    Update 136_Single Number.cpp

--- 101-150/136_Single Number.cpp
+++ 101-150/136_Single Number.cpp
@@ -17,6 +17,10 @@ Output: 4
 
 */
 
+// solutions:
+// 1. double pointer, compare two number each time
+// 2. hashMap, first insert, second erase, final exist one is result
+// 3. XOR of all numbers, result is single number value
 
 class Solution {
 public:
@@ -31,5 +35,45 @@ public:
             ret ^= item;
         }
         return ret;
+    }
+};
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // first idea is sort and double pointer
+        // O(NlogN)
+        if (nums.size() == 1)
+            return nums[0];
+        
+        sort(nums.begin(), nums.end());
+        
+        // notice we compare 2 at one time and skip two to avoid duplicated num
+        for (int i = 0, n = nums.size(); i < n-1; i += 2) {
+            if (nums[i] != nums[i+1]) {
+                // maybe i or i+1
+                if (i == 0) {
+                    return nums[i];
+                } else {
+                    return nums[i-1] == nums[i] ? nums[i+1] : nums[i];
+                }
+            }
+        }
+        
+        // if not return before, must be the last one
+        return nums.back();
+    }
+};
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_set<int> st;
+        for (int num : nums) {
+            if (st.count(num)) st.erase(num);
+            else st.insert(num);
+        }
+        return *st.begin();
     }
 };
