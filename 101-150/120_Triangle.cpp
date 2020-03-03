69da24c0ba8b96de8d0021c6bceab58bb0791c37
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-03 11:29:33 +0800

    Update 120_Triangle.cpp

--- 101-150/120_Triangle.cpp
+++ 101-150/120_Triangle.cpp
@@ -19,6 +19,19 @@ Bonus point if you are able to do this using only O(n) extra space, where n is t
 */
 
 
+// keypoints:
+// 1. greedy algorithm does not work, it will converge at local minimum
+// 2. using dynamic planning to find global minmum
+// 3. instead of searching from top to bottom, we search from bottom to top
+// 4. since last row has the maximum elements, we copy it as initial value of dp array
+// 5. dp[i-1][j] = min(dp[i][j], dp[i][j+1]) + triangle[i][j]
+//    using infomation from i-th row to update (i-1)-th row
+// 6. since we reuse dp array, it leads to:
+//    dp[j] = min(dp[j], dp[j+1]) + triangle[i][j]
+// 7. dp[0] is the minimum result
+
+
+
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
@@ -26,7 +39,7 @@ public:
         // think different, from down to top, left to right, 2 steps
         // 1. find min from cur idx and after idx(this is adjacent in row)
         // 2. plus same idx of prev row(this is adjacent in column)
-        // dp[i][j] = min(dp[i][j], dp[i][j+1]) + triangle[i][j]
+        // dp[i-1][j] = min(dp[i][j], dp[i][j+1]) + triangle[i][j]
         // since last row has the longest length, use it as dp length, which gives:
         // dp[j] = min(dp[j], dp[j+1]) + triangle[i][j]
         
