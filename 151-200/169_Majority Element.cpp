bc1ce7419ffc21ed66238fee2066d3771f049aad
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-04 16:11:42 +0800

    Create 169_Majority Element.cpp

---
+++ 151-200/169_Majority Element.cpp
@@ -0,0 +1,50 @@
+/*
+
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+
+*/
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        
+        return nums[nums.size()/2];
+    }
+};
+
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int cnt = 0;
+        int candidate = 0;
+        
+        // useful when and ONLY when more than n/2 repeaters exist
+        for (auto num : nums) {
+            if (cnt == 0) { // prev candidate is weak, since exist num occur times equal to it
+                candidate = num; // update candidate and cnt
+                ++cnt;
+            } else {
+                candidate == num ? ++cnt : --cnt;
+            }
+        }
+        
+        return candidate;
+    }
+};
