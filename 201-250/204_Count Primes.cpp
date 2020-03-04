5d4da55be3fb6b73bf763abf644f187a6fb224ce
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-04 21:18:29 +0800

    Update 204_Count Primes.cpp

--- 201-250/204_Count Primes.cpp
+++ 201-250/204_Count Primes.cpp
@@ -10,6 +10,11 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 
 */
 
+// keypoints: 
+// 1. prime > 1 && only divisible by 1 and itself
+// 2. find the first prime i, then mark all n*i as non-prime(n >= 2) 
+// 3. the first one un-marked is prime j, then mark all n*j as non-prime(n >= 2) 
+// 4. do the loop
 
 class Solution {
 public:
@@ -18,17 +23,17 @@ public:
         // then find the next prime j(the first unmark num), mark all j*n as non-prime
         // until n
         int cnt = 0;
-        // non-prime make as false, in fact mark[0] and mark[1] are false
-        // since we start from 2, mark[1] has no effect with result
-        vector<bool> mark(n, true); 
+        // non-prime mark as false
+        // since prime > 1, we start from 2
+        vector<bool> isPrime(n, true); 
         for (int i = 2; i < n; ++i) {
-            if (mark[i]) {
+            if (isPrime[i]) {
                 
                 ++cnt;
                 
                 // j = 1, i*j = i, nonsense
                 for (int j = 2; i*j < n; ++j) {
-                    mark[i*j] = false;
+                    isPrime[i*j] = false;
                 }
             }
         }
