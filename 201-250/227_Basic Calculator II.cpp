8acec4d0c7256a45113445805047d11dde8d5c4b
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-05 20:15:38 +0800

    Update 227_Basic Calculator II.cpp

--- 201-250/227_Basic Calculator II.cpp
+++ 201-250/227_Basic Calculator II.cpp
@@ -24,4 +24,60 @@ Do not use the eval built-in library function.
 */
 
 
+// keypoints:
+// 1. calulate result in one pass
+// 2. consider sign '+' and '-' as positive and negative
+// 3. careful about / and *
+// 4. use ASCII value to seperate nums and signs
+//    order in ASCII: space, bracket, sign, digits, lower character, capitial
+// 5. corner case: when i == n-1, we need to push the last num into stack!!
+// 6. use long type in case overflow
 
+class Solution {
+public:
+    int calculate(string s) {
+        if (s.size() == 0)
+            return 0;
+        
+        // init digit and prevSign
+        long num = 0;
+        char prev = '+';
+        stack<long> st;
+        
+        for (int i = 0, n = s.size(); i < n; ++i) {
+            
+            char &cur = s[i];
+            
+            if (cur >= '0' && cur <= '9') {
+                num = num*10 + cur - '0';
+            } 
+            
+            if ((cur < '0' &&  cur != ' ') || i == n-1) { // reach end also need to push num
+                
+                if (prev == '+') {
+                    st.push(num);
+                } else if (prev == '-') {
+                    st.push(-num);
+                } else if (prev == '*' || prev == '/') {
+                    long temp = st.top();
+                    st.pop();
+                    
+                    temp = prev == '*' ? temp*num : temp/num;
+                    st.push(temp);
+                }
+                
+                prev = cur;
+                num = 0;   
+            }
+        }
+        
+        long sum = 0;
+        while (!st.empty()) {
+            sum += st.top();
+            st.pop();
+        }
+        
+        return sum;
+        
+    }
+};
