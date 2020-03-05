df364a3cbdd556f401703ae4ae375f70d3163ef8
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-05 14:47:20 +0800

    Create 217_Contains Duplicate.cpp

---
+++ 201-250/217_Contains Duplicate.cpp
@@ -0,0 +1,44 @@
+/*
+
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+*/
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // use travial sort method
+        
+        for (int i = 1, n = nums.size(); i < n; ++i) {
+            int temp = nums[i];
+            int j = i;
+            while(j > 0 && nums[j-1] > temp) {
+                nums[j] = nums[j-1];
+                --j;
+            }
+            
+            if (j > 0 && nums[j-1] == temp)
+                return true;
+            
+            nums[j] = temp;
+        }
+        
+        return false;
+    }
+};
