82d39e21f41fd1a070278757c145c1054e10ec64
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-05 15:57:45 +0800

    Update 225_Implement Stack using Queues.cpp

--- 201-250/225_Implement Stack using Queues.cpp
+++ 201-250/225_Implement Stack using Queues.cpp
@@ -23,6 +23,22 @@ You may assume that all operations are valid (for example, no pop or top operati
 
 */
 
+// keypoints:
+// queue: FIFO, stack: FILO
+// (1) using 1 queue
+// 1. when push x, push x into queue
+// 2. repeat push(front) and pop (n-1) times, so nums before x now behind it
+// 2. pop and top and empty not change
+
+// (2) using 2 queue(use one queue to keep top element)
+// 1. when push x, push x into queue 2(queue 2 only keep top num, aka the last push num)
+//    if queue 2 size > 1, push queue 2 front to queue 1, queue 2 pop
+// 2. when pop, queue 2 return and pop
+// 3. when top, if queue 2 not empty, return front
+//    if empty, queue q repeat push(front) and pop (n-1) times, so nums before last num now behind it
+//    push queue 1 front to queue 2, then queue 1 pop
+// 4. if queue 1 and queue 2 botn empty, return empty
+
 
 // solution 1: use 1 queue
 
