f13140519c4d732e1827468be0cfdf404ae5b2aa
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-06 10:59:37 +0800

    Update 242_Valid Anagram.cpp

--- 201-250/242_Valid Anagram.cpp
+++ 201-250/242_Valid Anagram.cpp
@@ -18,7 +18,39 @@ What if the inputs contain unicode characters? How would you adapt your solution
 
 */
 
+// keypoints:
+// 1. hash table is a nature choise
+// 2. if only contains lowercase alphabets, we use int[26] as hash table
+// 3. contain unicode characters --> int [256]
 
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+        
+        unordered_map<char, int> hash;
+        int cnt = 0;
+        while (cnt < 26) {
+            hash['a'+cnt] = 0;
+            ++cnt;
+        }
+        
+        for (int i = 0, n = s.size(); i < n; ++i) {
+            ++hash[s[i]];
+            --hash[t[i]];
+        }
+        
+        for (auto it : hash) {
+            if (it.second)
+                return false;
+        }
+        
+        return true;
+    }
+};
 
 class Solution {
 public:
