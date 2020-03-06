d1464fee75df5aee512e444ff471906327197ce8
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-06 20:08:57 +0800

    Update 313_Super Ugly Number.cpp

--- 301-350/313_Super Ugly Number.cpp
+++ 301-350/313_Super Ugly Number.cpp
@@ -20,3 +20,35 @@ The nth super ugly number is guaranteed to fit in a 32-bit signed integer.
 */
 
 
+// keypoints:
+// 1. 1 is super Ugly number
+// 2. set idx for each prime
+// 3. nums[idx]*prime[idx], search minmum value as new prime(avoid duplicate)
+// 4. push new super ugly number into result
+// 5. increase idx we use last step
+// 6. stop when we get n numbers
+
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        int k = primes.size();
+        vector<int> idx(k, 0);
+        vector<int> nums({1});
+        
+        while (nums.size() < n) {
+            int cur = INT_MAX;
+            for (int i = 0; i < k; ++i) {
+                cur = min(cur, nums[idx[i]] * primes[i]);
+            }
+            
+            nums.push_back(cur);
+            
+            for (int i = 0; i < k; ++i) {
+                if (cur == nums[idx[i]] * primes[i])
+                    ++idx[i]; // may update more than one index, NOT use break
+            }
+        }
+        
+        return nums.back();
+    }
+};
