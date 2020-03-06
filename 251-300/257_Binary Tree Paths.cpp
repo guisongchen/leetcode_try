263df33c2a737f90727c48d023cb387311a0c025
guisongchen <48375352+guisongchen@users.noreply.github.com>
2020-03-06 11:03:17 +0800

    Create 257_Binary Tree Paths.cpp

---
+++ 251-300/257_Binary Tree Paths.cpp
@@ -0,0 +1,58 @@
+/*
+
+Given a binary tree, return all root-to-leaf paths.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input:
+
+   1
+ /   \
+2     3
+ \
+  5
+
+Output: ["1->2->5", "1->3"]
+
+Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if (!root)
+            return vector<string>{};
+        if (!root->left && !root->right)
+            return vector<string>{to_string(root->val)};
+        
+        vector<string> left = binaryTreePaths(root->left);
+        vector<string> right = binaryTreePaths(root->right);
+        
+        string cur = to_string(root->val);
+        vector<string> ret;
+        for (auto it : left) {
+            string temp = cur + "->" + it;
+            ret.push_back(temp);
+        }
+        for (auto it : right) {
+            string temp = cur + "->" + it;
+            ret.push_back(temp);
+        }
+        
+        return ret;
+    }
+};
